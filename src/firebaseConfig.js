de1fe2691ef8bd8fc28991629de2fcf94a0be215
leal <jorge.leal@cceo.com.mx>
2023-02-23 10:38:32 -0600

    Configuración Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkeihVWY3g0iZjA6I4kBN-0tZJYOr3I-A",
+  authDomain: "vue-firebase-294bc.firebaseapp.com",
+  projectId: "vue-firebase-294bc",
+  storageBucket: "vue-firebase-294bc.appspot.com",
+  messagingSenderId: "695619983826",
+  appId: "1:695619983826:web:4a140e407e9fffd85d900b"
+};
+
+// Initialize Firebase
+// -- ---------------- const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
